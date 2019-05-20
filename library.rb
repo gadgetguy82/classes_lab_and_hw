eeedb56de494f4a104f13ab181ac7abd8de55341
Robert Tam <rsh.tam@gmail.com>
2019-05-20 15:25:48 +0100

    Add final functions and tests for Library class

--- library.rb
+++ library.rb
@@ -6,5 +6,39 @@ class Library
     @books = book_list
   end
 
+  def book_details(title)
+    for book in @books
+      if (book[:title].downcase == title.downcase)
+        return book
+      end
+    end
+    return nil
+  end
 
+  def rental_details(title)
+    for book in @books
+      if (book[:title].downcase == title.downcase)
+        return book[:rental_details]
+      end
+    end
+    return nil
+  end
+
+  def add_book(new_title)
+    @books.push(
+      {
+        title: new_title,
+        rental_details: {
+          student_name: '',
+          due_date: ''
+        }
+      }
+    )
+  end
+
+  def change_rental_details(title, student, date)
+    details = rental_details(title)
+    details[:student_name] = student
+    details[:due_date] = date
+  end
 end
--- specs/library_specs.rb
+++ specs/library_specs.rb
@@ -30,10 +30,38 @@ class TestLibrary < Minitest::Test
     }
 
     @book_list = [@book1, @book2, @book3]
+
+    @my_library = Library.new(@book_list)
   end
 
   def test_library_book_getters
-    my_library = Library.new(@book_list)
-    assert_equal(@book2, my_library.books[1])
+    assert_equal(@book2, @my_library.books[1])
   end
+
+  def test_get_all_book_details_from_title
+    assert_equal(@book3, @my_library.book_details('Treasure Island'))
+  end
+
+  def test_get_rental_details_from_title
+    assert_equal(@book1[:rental_details], @my_library.rental_details('Lord of the Rings'))
+  end
+
+  def test_add_book_to_library
+    @my_library.add_book('War and Peace')
+    assert_equal(4, @my_library.books.length)
+  end
+
+  def test_change_rental_details
+    @my_library.change_rental_details('Treasure Island', 'Peter', '04/08/19')
+    assert_equal(
+      {
+        title: 'Treasure Island',
+        rental_details: {
+          student_name: 'Peter',
+          due_date: '04/08/19'
+        }
+      }, @my_library.books[2]
+    )
+  end
+
 end
